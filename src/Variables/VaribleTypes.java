516e3efee9623278b7c935fc44115098f79e75a4
Devin1996 <devinchandula@gmail.com>
2021-09-20 00:56:52 +0530

    Variables

---
+++ src/Variables/VaribleTypes.java
@@ -0,0 +1,15 @@
+package Variables;
+
+	public class VaribleTypes 
+	{  
+	    static int m=100;//static variable  
+	    void method()  
+	    {    
+	        int n=90;//local variable  
+	        System.out.print(n);
+	    }  
+	    public static void main(String args[])  
+	    {  
+	        int data=50;//instance variable    
+	    }  
+	}//end of class   
