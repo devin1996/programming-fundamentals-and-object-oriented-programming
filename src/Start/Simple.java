22ee8f83d4e2330d6a52eadab377b9613b0cad92
Devin1996 <devinchandula@gmail.com>
2021-09-19 20:24:11 +0530

    First code

--- src/Start/Simple.java
+++ src/Start/Simple.java
@@ -2,6 +2,8 @@ package Start;
 
 class Simple{  
 	    public static void main(String args[]){  
-	     System.out.println("Hello Java");  
+	     System.out.println("Hello Java");
+	     System.out.println("Picture abhi baaki hai mere dost");
+	     
 	    }  
 	}  
