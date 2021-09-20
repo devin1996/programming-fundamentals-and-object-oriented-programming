376f57fe0a72fc70af432f53f3abcb8344aa15ce
Devin1996 <devinchandula@gmail.com>
2021-09-20 09:48:48 +0530

    Widening and Narrowing

---
+++ src/Variables/Narrowing.java
@@ -0,0 +1,14 @@
+package Variables;
+
+public class Narrowing {
+	
+	public static void main(String[] args){  
+		float f=10.5f;  
+		//int a=f;//Compile time error  
+		int a=(int)f; //type casting  
+		System.out.println(f);  
+		System.out.println(a);  
+
+
+ }
+}
---
+++ src/Variables/Widening.java
@@ -0,0 +1,15 @@
+package Variables;
+
+public class Widening {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;  
+		float f=a;  
+		System.out.println(a);  
+		System.out.println(f);  //10.0
+
+
+	}
+
+}
