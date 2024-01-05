a64758f9dc7d738a77277d0ec9a0eafede13db55
Devin <devinchandula@gmail.com>
2024-01-05 11:57:42 +0530

    Hello World
    Signed-off-by: devin1996 <devinchandula@gmail.com>

---
+++ cpp/ConsoleApplication/HelloThere.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class HelloThere {
+
+public:
+	bool HelloWorld() {
+		cout << "Hello World!\n" << endl;
+		return 0;
+	}
+
+
+	HelloThere() = default;
+};
+
+
